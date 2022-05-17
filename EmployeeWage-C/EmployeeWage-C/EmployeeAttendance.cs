63e500bc8c84f8b6af52a007cfd48ea0281933de
Reshma2310 <dreshma253@gmail.com>
2022-05-17 10:20:06 +0500

    [Reshma] - Employee Attendance

---
+++ EmployeeWage-C/EmployeeWage-C/EmployeeAttendance.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage_C
+{
+    internal class EmployeeAttendance
+    {
+        Random random = new Random();
+        public void AttendanceCheck()
+        {
+            if (random.Next(0,2) == 0)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
---
+++ EmployeeWage-C/EmployeeWage-C/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+EmployeeWage_C.EmployeeAttendance attendance = new EmployeeWage_C.EmployeeAttendance();
+attendance.AttendanceCheck();
