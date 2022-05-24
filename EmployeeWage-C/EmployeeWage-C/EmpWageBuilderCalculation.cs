7d9eac3d2bb3ffd1c82a9edb38167d351f44f3da
Reshma2310 <dreshma253@gmail.com>
2022-05-24 10:41:22 +0500

    [Reshma] - Saving Total Wage for Each Company

---
+++ EmployeeWage-C/EmployeeWage-C/EmpWageBuilderCalculation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage_C
+{
+    internal class EmpWageBuilderObject
+    {
+        public const int isPartTime = 1, isFullTime = 2;
+        private string company;
+        private int empRatePerHour, numOfWorkingDays, maxHoursPerMonth, totalEmpWage;
+        public EmpWageBuilderObject(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+        }
+        public void computeEmpWage()
+        {
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            while (totalEmpHrs <= this.maxHoursPerMonth && totalWorkingDays < this.numOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case isPartTime:
+                        empHrs = 4;
+                        break;
+                    case isFullTime:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day: " + totalWorkingDays + "Employee Hours: " + empHrs);
+            }
+            totalEmpWage = totalEmpHrs * this.empRatePerHour;
+            Console.WriteLine("Total Employee Wage for Company {0} is {1}", company, totalEmpWage);
+        }
+        public string toString()
+        {
+            return "Total Employee Wage for company: " + this.company + "is : " + totalEmpWage;
+        }
+
+
+    }
+}
--- EmployeeWage-C/EmployeeWage-C/Program.cs
+++ EmployeeWage-C/EmployeeWage-C/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 
 Console.WriteLine("Welcome to Employee Wage Program");
-EmployeeWage_C.EmployeeAttendance company1 = new EmployeeWage_C.EmployeeAttendance();
-EmployeeWage_C.EmployeeAttendance company2 = new EmployeeWage_C.EmployeeAttendance();
-company1.EmployeeWages();
-company2.EmployeeWages();
+EmployeeWage_C.EmpWageBuilderObject dMart = new EmployeeWage_C.EmpWageBuilderObject("DMart", 20, 6, 100);
+EmployeeWage_C.EmpWageBuilderObject reliance = new EmployeeWage_C.EmpWageBuilderObject("Reliance", 18, 12, 120);
+dMart.computeEmpWage();
+Console.WriteLine(dMart.toString());
+reliance.computeEmpWage();
+Console.WriteLine(reliance.toString());
