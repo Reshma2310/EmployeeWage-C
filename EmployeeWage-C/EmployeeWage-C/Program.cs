5e4e42bd1583cacd304d72c895b5d41da1a66519
Reshma2310 <dreshma253@gmail.com>
2022-05-24 12:40:54 +0500

    [Reshma] - Saving Total Wage for Each Company

--- EmployeeWage-C/EmployeeWage-C/Program.cs
+++ EmployeeWage-C/EmployeeWage-C/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using EmployeeWage_C;
 
 Console.WriteLine("Welcome to Employee Wage Program");
-EmployeeWage_C.EmpWageBuilderObject dMart = new EmployeeWage_C.EmpWageBuilderObject("DMart", 20, 6, 100);
-EmployeeWage_C.EmpWageBuilderObject reliance = new EmployeeWage_C.EmpWageBuilderObject("Reliance", 18, 12, 120);
+EmpWageBuilderObject dMart = new EmpWageBuilderObject("DMart", 20, 6, 100);
+EmpWageBuilderObject reliance = new EmpWageBuilderObject("Reliance", 18, 12, 120);
 dMart.computeEmpWage();
 Console.WriteLine(dMart.toString());
 reliance.computeEmpWage();
